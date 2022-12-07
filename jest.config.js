36493eff5fd24168871a7c877bd5254efd7e02de
Vid <vid_github@zooid.org>
2022-12-07 10:25:42 -0500

    cleanup

--- jest.config.js
+++ jest.config.js
@@ -1,5 +1,12 @@
+"use strict";
 /** @type {import('ts-jest/dist/types').InitialOptionsTsJest} */
 module.exports = {
-  preset: 'ts-jest',
-  testEnvironment: 'node',
+    preset: 'ts-jest',
+    testEnvironment: 'node',
+    testPathIgnorePatterns: ['node_modules', 'build'],
+    globals: {
+        'testMatch': [
+            "<rootDir>/src/**/*.test.ts"
+        ]
+    }
 };
